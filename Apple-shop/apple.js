02c62c4b3636b290022c77f97b0a75b132e5085a
HabibullohUz <darvesh1304@gmail.com>
2022-11-12 14:11:09 +0500

    apple-project news

--- Apple-shop/apple.js
+++ Apple-shop/apple.js
@@ -24,7 +24,7 @@ const apple_products = [
           price: 529.99,
      },
      {
-          img: "./assets/products/Image-4.jpg",
+          img: "./assets/products/Image-2.jpg",
           title: "Apple Watch",
           type: "Series 5 SE",
           price: 529.99,
@@ -48,7 +48,31 @@ const apple_products = [
           price: 529.99,
      },
      {
-          img: "./assets/products/Image-4.jpg",
+          img: "./assets/products/Image-6.jpg",
+          title: "Apple Watch",
+          type: "Series 5 SE",
+          price: 529.99,
+     },
+     {
+          img: "./assets/products/Image-2.jpg",
+          title: "Apple Watch",
+          type: "Series 5 SE",
+          price: 529.99,
+     },
+     {
+          img: "./assets/products/Image-3.jpg",
+          title: "Apple Watch",
+          type: "Series 5 SE",
+          price: 529.99,
+     },
+     {
+          img: "./assets/products/Image-2.jpg",
+          title: "Apple Watch",
+          type: "Series 5 SE",
+          price: 529.99,
+     },
+     {
+          img: "./assets/products/Image-3.jpg",
           title: "Apple Watch",
           type: "Series 5 SE",
           price: 529.99,
@@ -70,7 +94,6 @@ const apple_products = [
 
 // const contentFrame = document.querySelector('.content_frame')
 
-
 function showAppleProducts() {
      const content_frame = document.querySelector('.content_frame')
 
@@ -78,15 +101,25 @@ function showAppleProducts() {
 
           content_frame.innerHTML += `
                <div class="card">
-                 <img src="${product.img}" alt="">
+                 <a href="./product-view.html"><img  class="product-img" src="${product.img}" alt=""></a>
                  <div class="card-body">
                    <h5 class="product_title">${product.title}</h5>
                    <p class="product_type">${product.type}</p>
-                   <p class="product_price">${product.price}</p>
-                   <a href="#" class="btn btn-dark"><img src="./assets/add-bag.svg" alt=""></a>
+                   <div class="add__product-container">
+                        <p class="product_price">$ ${product.price}</p>
+                        <a href="#" class="btn btn__add-product"><img src="./assets/add-bag.svg" alt=""></a>
+                   </div>
                  </div>
                </div>
           `
      })
 }
 showAppleProducts()
+new Date(2000)
+
+     ```
+- keep it between us ===> buni oramizda saqla!
+- you just blew my mind ===> sen menu shunchaki hayron qoldirding!
+- don't bite more than you can chew ===> tishlaganingdan ortig'ini chaynama
+- Siroj works hard to put food on the table for his family
+```
